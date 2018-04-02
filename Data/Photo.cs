dc818737395f2f96aae7542310c6e6186f40d372
nikitakuzya <nikitakuzy@gmail.com>
2018-04-03 00:32:27 +0300

    tets

---
+++ Data/Photo.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace MyPhotoshop
+{
+	public class Photo
+	{
+		public int width;
+		public int height;
+		public Pixel [,] data;
+	}
+    
+}
+
---
+++ Data/Pixel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyPhotoshop
+{
+    public class Pixel
+    {
+        double r;
+        double g;
+        double b;
+
+        public double Check(double value)
+        {
+            if (value < 0 || value > 1) throw new ArgumentException();
+            return value;
+        }
+
+        public double R
+        {
+            get { return r; }
+            set
+            {
+                r = Check(value);
+            }
+        }
+        public double G
+        {
+            get { return g; }
+            set
+            {
+                r = Check(value);
+            }
+        }
+        public double B
+        {
+            get { return b; }
+            set
+            {
+                r = Check(value);
+            }
+        }
+    }
+}
